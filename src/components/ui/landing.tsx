280c1735d88ba7aeb4c8914d3a3dc427a48c2f1f
Jack Colpitt <jackc@srowners.org>
2025-08-04 16:18:36 -0700

    contact

--- src/components/ui/landing.tsx
+++ src/components/ui/landing.tsx
@@ -1,10 +1,14 @@
 export default function LandingPage() {
     return (
         <div>
-            <div className="text-center text-7xl font-manrope font-bold bg-gradient-to-b from-slate-900 to-slate-200 bg-clip-text text-transparent text-shadow-2xs">
-            Colpitt Land & <br /> Data LLC
+            <div
+                className="text-center text-7xl font-manrope font-bold bg-gradient-to-b from-slate-900 to-slate-200 bg-clip-text text-transparent text-shadow-2xs">
+                Colpitt Land & <br/> Data LLC
             </div>
-            <div className="my-10 font-manrope text-slate-900 text-shadow-2xs">
+            <div className="m-5 font-manrope text-slate-900 text-shadow-2xs">
+                Contact us @ landdatallc@gmail.com
+            </div>
+            <div className="m-2 font-manrope text-slate-900 text-shadow-2xs">
                 More details coming soon...
             </div>
         </div>
