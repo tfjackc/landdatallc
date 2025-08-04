6ef94aed7015272571c0e7491ab440dfbfa7b93a
Jack Colpitt <jackc@srowners.org>
2025-08-04 16:11:28 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LandingPage from "@/components/ui/landing.tsx";
+function App() {
+
+  return (
+      <>
+          <div className="bg-gradient-to-t from-slate-50 to-zinc-200 min-h-screen flex items-center justify-center">
+                <LandingPage />
+          </div>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/ui/landing.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <div className="text-center text-7xl font-manrope font-bold bg-gradient-to-b from-slate-900 to-slate-200 bg-clip-text text-transparent text-shadow-2xs">
+            Colpitt Land & <br /> Data LLC
+        </div>
+    )
+}
